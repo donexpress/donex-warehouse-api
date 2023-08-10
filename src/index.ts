8266f50a5e8c21a50a39d5af72552a3e725641e0
YMC <ycuello85@gmail.com>
2023-08-10 14:20:39 -0400

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,43 @@
-import 'dotenv/config'
-import express, { Express } from 'express';
-import morgan from 'morgan';
-import { AppDataSource } from "./config/ormconfig"
-import { AffiliationRoutes, AffiliationStateRoutes, DeparmentRoutes, LoginRoutes, RoleRoutes, UserRoutes, UserStateRoutes } from './routes';
-
+import "dotenv/config";
+import express, { Express } from "express";
+import morgan from "morgan";
+import { AppDataSource } from "./config/ormconfig";
+import {
+  AffiliationRoutes,
+  AffiliationStateRoutes,
+  DeparmentRoutes,
+  LoginRoutes,
+  RoleRoutes,
+  UserRoutes,
+  UserStateRoutes,
+} from "./routes";
 
 const app: Express = express();
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3000;
 
 /** Midlewares */
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-
-AppDataSource.initialize().then(() => {
-    console.log(`Database sync successfully`)
-}).catch(error => console.log(error))
+AppDataSource.initialize()
+  .then(() => {
+    console.log(`Database sync successfully`);
+  })
+  .catch((error) => console.log(error));
 
 /** Routes */
-app.use('/', DeparmentRoutes.default);
-app.use('/', UserStateRoutes.default);
-app.use('/', RoleRoutes.default);
-app.use('/', AffiliationRoutes.default);
-app.use('/', AffiliationStateRoutes.default);
-app.use('/', UserStateRoutes.default);
-app.use('/', UserRoutes.default);
-app.use('/', LoginRoutes.default);
+app.use("/", DeparmentRoutes.default);
+app.use("/", UserStateRoutes.default);
+app.use("/", RoleRoutes.default);
+app.use("/", AffiliationRoutes.default);
+app.use("/", AffiliationStateRoutes.default);
+app.use("/", UserStateRoutes.default);
+app.use("/", UserRoutes.default);
+app.use("/", LoginRoutes.default);
 
-app.get('/api/v1', (req, res) => res.sendStatus(200));
+app.get("/api/v1", (req, res) => res.sendStatus(200));
 
-app.listen(PORT, ()=> {
-    console.log(`Service running on port: ${PORT}`)
-})
-
-
-
-
-
+app.listen(PORT, () => {
+  console.log(`Service running on port: ${PORT}`);
+});
