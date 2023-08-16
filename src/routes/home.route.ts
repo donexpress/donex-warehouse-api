9917ec0ddb228d3cc82e727e31ab96575902c57b
YMC <ycuello85@gmail.com>
2023-08-16 13:37:09 -0400

    save

--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -3,6 +3,6 @@ import { HomeController } from "../controllers";
 import { verifyTokenPresent } from "../middlewares";
 const router = express.Router();
 
-router.get("/api/v1/countries", verifyTokenPresent, HomeController.country);
+router.get("/api/v1/countries", HomeController.country);
 
 export default router;
