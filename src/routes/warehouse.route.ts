26e9d98a870e321c3b54b54b55676afb6ca8e421
YMC <ycuello85@gmail.com>
2023-08-16 18:35:39 -0400

    save

--- src/routes/warehouse.route.ts
+++ src/routes/warehouse.route.ts
@@ -3,15 +3,15 @@ import { WarehouseController } from "../controllers";
 import { verifyTokenPresent } from "../middlewares";
 const router = express.Router();
 
-router.get("/api/v1/warehouse", WarehouseController.index);
+router.get("/api/v1/warehouse", verifyTokenPresent, WarehouseController.index);
 router.get("/api/v1/warehouse/count", WarehouseController.count);
 router.get("/api/v1/warehouse/:id", WarehouseController.show);
-router.post(
-  "/api/v1/warehouse",
-  verifyTokenPresent,
-  WarehouseController.create
-);
+router.post("/api/v1/warehouse", WarehouseController.create);
 router.put("/api/v1/warehouse/:id", WarehouseController.update);
-router.delete("/api/v1/warehouse/:id", WarehouseController.remove);
+router.delete(
+  "/api/v1/warehouse/:id",
+  verifyTokenPresent,
+  WarehouseController.remove
+);
 
 export default router;
