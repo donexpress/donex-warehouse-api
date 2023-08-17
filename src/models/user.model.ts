003eb666fab10111c58607a5f471c3bee4a13c7c
YMC <ycuello85@gmail.com>
2023-08-17 16:46:47 -0400

    save

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -79,65 +79,65 @@ export class User {
     nullable: false,
   })
   //@IsOptional()
-  @IsInt()
-  @Min(1)
-  state_id: number;
+  //@IsInt()
+  //@Min(1)
+  state_id: number | null;
 
   @Column({
     type: 'integer',
     nullable: false,
   })
-  @IsInt()
-  @Min(1)
-  finantial_representative: number;
+  //@IsInt()
+  //@Min(1)
+  finantial_representative: number | null;
 
   @Column({
     type: 'integer',
     nullable: false,
   })
-  @IsInt()
-  @Min(1)
-  client_service_representative: number;
+  //@IsInt()
+  //@Min(1)
+  client_service_representative: number | null;
 
   @Column({
     type: 'integer',
     nullable: false,
   })
-  @IsInt()
-  @Min(1)
-  sales_representative: number;
+  //@IsInt()
+  //@Min(1)
+  sales_representative: number | null;
 
   @Column({
     type: 'integer',
     nullable: false,
   })
-  @IsInt()
-  @Min(1)
-  sales_source: number;
+  //@IsInt()
+  //@Min(1)
+  sales_source: number | null;
 
   @Column({
     type: 'integer',
     nullable: false,
   })
-  @IsInt()
-  @Min(1)
-  subsidiary_id: number;
+  //@IsInt()
+  //@Min(1)
+  subsidiary_id: number | null;
 
   @Column({
     type: 'integer',
     nullable: false,
   })
-  @IsInt()
-  @Min(1)
-  regional_division_id: number;
+  //@IsInt()
+  //@Min(1)
+  regional_division_id: number | null;
 
   @Column({
     type: 'integer',
     nullable: true,
   })
-  @IsInt()
-  @Min(1)
-  warehouse_id: number;
+  //@IsInt()
+  //@Min(1)
+  warehouse_id: number | null;
 
   @Column({
     type: 'boolean',
@@ -161,61 +161,61 @@ export class User {
     type: 'integer',
     nullable: false,
   })
-  @IsInt()
-  @Min(1)
-  user_level_id: number;
+  //@IsInt()
+  //@Min(1)
+  user_level_id: number | null;
 
   @Column({
     type: 'integer',
     nullable: false,
   })
-  @IsInt()
-  @Min(1)
-  payment_method_id: number;
+  //@IsInt()
+  //@Min(1)
+  payment_method_id: number | null;
 
   @ManyToOne((type) => UserState, (state) => state.users)
   @JoinColumn({ name: 'state_id', referencedColumnName: 'id' })
-  states: UserState;
+  states: UserState | null;
 
   @ManyToOne((type) => Staff, (staff) => staff.finantial_representatives)
   @JoinColumn({ name: 'finantial_representative', referencedColumnName: 'id' })
-  finantial_representatives: Staff;
+  finantial_representatives: Staff | null;
 
   @ManyToOne((type) => Staff, (staff) => staff.client_service_representative)
   @JoinColumn({
     name: 'client_service_representative',
     referencedColumnName: 'id',
   })
-  client_service_representatives: Staff;
+  client_service_representatives: Staff | null;
 
   @ManyToOne((type) => Staff, (staff) => staff.sales_representatives)
   @JoinColumn({ name: 'sales_representative', referencedColumnName: 'id' })
-  sales_representatives: Staff;
+  sales_representatives: Staff | null;
 
   @ManyToOne((type) => Staff, (staff) => staff.sales_sources)
   @JoinColumn({ name: 'sales_source', referencedColumnName: 'id' })
-  sales_sources: Staff;
+  sales_sources: Staff | null;
 
   @ManyToOne((type) => Subsidiary, (subsidiary) => subsidiary.users)
   @JoinColumn({ name: 'subsidiary_id', referencedColumnName: 'id' })
-  subsidiaries: Subsidiary;
+  subsidiaries: Subsidiary | null;
 
   @ManyToOne(
     (type) => RegionalDivision,
     (regionalDivision) => regionalDivision.users
   )
   @JoinColumn({ name: 'regional_division_id', referencedColumnName: 'id' })
-  regional_divisions: RegionalDivision;
+  regional_divisions: RegionalDivision | null;
 
   @ManyToOne((type) => Warehouse, (warehouse) => warehouse.users)
   @JoinColumn({ name: 'warehouse_id', referencedColumnName: 'id' })
-  warehouses: RegionalDivision;
+  warehouses: RegionalDivision | null;
 
   @ManyToOne((type) => UserLevel, (user_level) => user_level.users)
   @JoinColumn({ name: 'user_level_id', referencedColumnName: 'id' })
-  user_level: UserLevel;
+  user_level: UserLevel | null;
 
   @ManyToOne((type) => PaymentMethod, (payment_method) => payment_method.users)
   @JoinColumn({ name: 'payment_method_id', referencedColumnName: 'id' })
-  payment_methods: PaymentMethod;
+  payment_methods: PaymentMethod | null;
 }
