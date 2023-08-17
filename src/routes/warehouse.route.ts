e960a41c13857c4a0ee7a3f45df793d59357ffcc
Jose Ruben <jose.ruben.machado.vega@gmail.com>
2023-08-17 14:04:20 -0400

    change warehouse route name to cargo_station

--- src/routes/warehouse.route.ts
+++ src/routes/warehouse.route.ts
@@ -3,11 +3,11 @@ import { WarehouseController } from "../controllers";
 import { verifyTokenPresent } from "../middlewares";
 const router = express.Router();
 
-router.get("/api/v1/warehouse", verifyTokenPresent, WarehouseController.index);
-router.get("/api/v1/warehouse/count", WarehouseController.count);
-router.get("/api/v1/warehouse/:id", WarehouseController.show);
-router.post("/api/v1/warehouse", WarehouseController.create);
-router.put("/api/v1/warehouse/:id", WarehouseController.update);
+router.get("/api/v1/cargo_station", verifyTokenPresent, WarehouseController.index);
+router.get("/api/v1/cargo_station/count", WarehouseController.count);
+router.get("/api/v1/cargo_station/:id", WarehouseController.show);
+router.post("/api/v1/cargo_station", WarehouseController.create);
+router.put("/api/v1/cargo_station/:id", WarehouseController.update);
 router.delete(
   "/api/v1/warehouse/:id",
   verifyTokenPresent,
