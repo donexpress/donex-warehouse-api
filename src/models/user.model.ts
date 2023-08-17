67bbd34c8ac6729db2b6fb68b4cd50ca59ba6856
YMC <ycuello85@gmail.com>
2023-08-17 17:25:35 -0400

    save

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -81,7 +81,7 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  state_id: number | null;
+  state_id: number;
 
   @Column({
     type: 'integer',
@@ -90,7 +90,7 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  finantial_representative: number | null;
+  finantial_representative: number;
 
   @Column({
     type: 'integer',
@@ -99,7 +99,7 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  client_service_representative: number | null;
+  client_service_representative: number;
 
   @Column({
     type: 'integer',
@@ -108,7 +108,7 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  sales_representative: number | null;
+  sales_representative: number;
 
   @Column({
     type: 'integer',
@@ -117,7 +117,7 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  sales_source: number | null;
+  sales_source: number;
 
   @Column({
     type: 'integer',
@@ -126,7 +126,7 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  subsidiary_id: number | null;
+  subsidiary_id: number;
 
   @Column({
     type: 'integer',
@@ -135,7 +135,7 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  regional_division_id: number | null;
+  regional_division_id: number;
 
   @Column({
     type: 'integer',
@@ -144,7 +144,7 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  warehouse_id: number | null;
+  warehouse_id: number;
 
   @Column({
     type: 'boolean',
@@ -171,7 +171,7 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  user_level_id: number | null;
+  user_level_id: number;
 
   @Column({
     type: 'integer',
@@ -180,51 +180,51 @@ export class User {
   @IsOptional()
   @IsInt()
   @Min(1)
-  payment_method_id: number | null;
+  payment_method_id: number;
 
   @ManyToOne((type) => UserState, (state) => state.users)
   @JoinColumn({ name: 'state_id', referencedColumnName: 'id' })
-  states: UserState | null;
+  states: UserState;
 
   @ManyToOne((type) => Staff, (staff) => staff.finantial_representatives)
   @JoinColumn({ name: 'finantial_representative', referencedColumnName: 'id' })
-  finantial_representatives: Staff | null;
+  finantial_representatives: Staff;
 
   @ManyToOne((type) => Staff, (staff) => staff.client_service_representative)
   @JoinColumn({
     name: 'client_service_representative',
     referencedColumnName: 'id',
   })
-  client_service_representatives: Staff | null;
+  client_service_representatives: Staff;
 
   @ManyToOne((type) => Staff, (staff) => staff.sales_representatives)
   @JoinColumn({ name: 'sales_representative', referencedColumnName: 'id' })
-  sales_representatives: Staff | null;
+  sales_representatives: Staff;
 
   @ManyToOne((type) => Staff, (staff) => staff.sales_sources)
   @JoinColumn({ name: 'sales_source', referencedColumnName: 'id' })
-  sales_sources: Staff | null;
+  sales_sources: Staff;
 
   @ManyToOne((type) => Subsidiary, (subsidiary) => subsidiary.users)
   @JoinColumn({ name: 'subsidiary_id', referencedColumnName: 'id' })
-  subsidiaries: Subsidiary | null;
+  subsidiaries: Subsidiary;
 
   @ManyToOne(
     (type) => RegionalDivision,
     (regionalDivision) => regionalDivision.users
   )
   @JoinColumn({ name: 'regional_division_id', referencedColumnName: 'id' })
-  regional_divisions: RegionalDivision | null;
+  regional_divisions: RegionalDivision;
 
   @ManyToOne((type) => Warehouse, (warehouse) => warehouse.users)
   @JoinColumn({ name: 'warehouse_id', referencedColumnName: 'id' })
-  warehouses: RegionalDivision | null;
+  warehouses: RegionalDivision;
 
   @ManyToOne((type) => UserLevel, (user_level) => user_level.users)
   @JoinColumn({ name: 'user_level_id', referencedColumnName: 'id' })
-  user_level: UserLevel | null;
+  user_level: UserLevel;
 
   @ManyToOne((type) => PaymentMethod, (payment_method) => payment_method.users)
   @JoinColumn({ name: 'payment_method_id', referencedColumnName: 'id' })
-  payment_methods: PaymentMethod | null;
+  payment_methods: PaymentMethod;
 }
