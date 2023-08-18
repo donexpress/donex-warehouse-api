054f937f7a42c017a5bd3b7fd3a1718655480d0a
Jose Ruben <jose.ruben.machado.vega@gmail.com>
2023-08-18 19:05:23 -0400

    change migration name

--- src/migrations/warehouse/1692395896931-change_cargo_station_state_id_not_null.ts
+++ src/migrations/warehouse/1692395896931-change_cargo_station_state_id_not_null.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, TableColumn } from "typeorm"
 
-export class CahngeCargoStationStateIdNotNull1692395896931 implements MigrationInterface {
+export class ChangeCargoStationStateIdNotNull1692395896931 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.changeColumn('warehouses', 'state_id', new TableColumn({
