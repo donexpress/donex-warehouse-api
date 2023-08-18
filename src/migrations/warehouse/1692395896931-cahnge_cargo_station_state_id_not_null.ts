cde4730921c911a1ba2f1b9509129ac2c9e9839c
Jose Ruben <jose.ruben.machado.vega@gmail.com>
2023-08-18 18:12:07 -0400

    adding missing column null

---
+++ src/migrations/warehouse/1692395896931-cahnge_cargo_station_state_id_not_null.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm"
+
+export class CahngeCargoStationStateIdNotNull1692395896931 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.changeColumn('warehouses', 'state_id', new TableColumn({
+            name: 'state_id',
+            type: 'integer',
+            isNullable: true
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.changeColumn('warehouses', 'state_id', new TableColumn({
+            name: 'state_id',
+            type: 'integer',
+            isNullable: false
+        }))
+    }
+
+}
