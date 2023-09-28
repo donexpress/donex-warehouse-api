ec991fec3447d417f1d3fe146756c54b76b41cd9
YMC <ycuello85@gmail.com>
2023-09-28 10:42:20 -0400

    wip

--- src/helpers/excel.ts
+++
@@ -1,8 +0,0 @@
-import * as path from 'path';
-import Excel from 'exceljs';
-
-const getCellValue = (row: Excel.Row, cellIndex: number) => {
-  const cell = row.getCell(cellIndex);
-
-  return cell.value ? cell.value.toString() : '';
-};
