fbbd9910e484caf7395e218eb4d739bc68b55678
YMC <ycuello85@gmail.com>
2023-09-28 11:11:22 -0400

    wip

--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -1,7 +1,7 @@
 import { validate } from 'class-validator';
 import { DataSource } from 'typeorm';
 
-export const validateContext = async (obj: DataSource, model) => {
+export const validateContext = async (obj, model) => {
   const errors = await validate(model);
   if (errors.length > 0) {
     return errors;
