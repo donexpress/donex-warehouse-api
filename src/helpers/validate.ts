c80a2704a4b4600fb009c85163764b3b156ebf71
YMC <ycuello85@gmail.com>
2023-09-28 11:22:48 -0400

    wip

--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -1,13 +1,10 @@
-import { validate } from 'class-validator';
-import { DataSource } from 'typeorm';
+import { validate } from "class-validator";
 
 export const validateContext = async (obj, model) => {
   const errors = await validate(model);
   if (errors.length > 0) {
     return errors;
   } else {
-    await obj.manager.transaction(async (transactionalEntityManager) => {
-      return await transactionalEntityManager.save(model);
-    });
+    return await obj.manager.save(model);
   }
 };
