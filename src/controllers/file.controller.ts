891d0f712711644f71c097e05f50ce7c69d1bcd5
Jose Ruben <jose.ruben.machado.vega@gmail.com>
2023-10-20 11:31:11 -0400

    move upload to helper file

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,51 +1,19 @@
 import { Request, Response } from 'express';
-import Busboy from 'busboy';
-import os from 'os';
-import path from 'path';
-import fs from 'fs';
-import { randomStr } from '../helpers';
-import { removeFile, uploadFileToStore } from '../context/file';
+import { removeFile } from '../context/file';
+import { upload } from '../helpers/file';
 
 export const upload_file = async (req: Request, res: Response) => {
   const contentLength = parseInt(req.headers['content-length'], 10);
-  let saveToPath = null;
-  let extension = null;
   if (contentLength >= 5 * 1024 * 1024) {
     res.status(413).send('Upload exceeds max size');
   }
-  const busboy = Busboy({
-    headers: req.headers,
-    limits: {
-      fileSize: 5 * 1024 * 1024,
-    },
-  });
-  busboy
-    .on(
-      'file',
-      function (
-        _name: string,
-        file: NodeJS.ReadableStream,
-        info: { filename: string; encoding: string; mimeType: string }
-      ) {
-        const randomFilename = randomStr(32);
-        extension = info.mimeType.split('/')[1];
-        saveToPath = path
-          .join(os.tmpdir(), randomFilename + '.' + extension)
-          .toString();
-        file.pipe(fs.createWriteStream(saveToPath));
-      }
-    )
-    .on('finish', async function () {
-      try {
-        const urls = await uploadFileToStore(saveToPath, extension);
-        res.status(201).send(urls);
-        fs.unlink(saveToPath, () => {});
-      } catch (e) {
-        console.error(e.toString());
-        res.sendStatus(422);
-      }
-    });
-  return req.pipe(busboy);
+  const result = upload(req, (urls: any) => {
+    if(urls === null)  {
+      return res.status(422).json(urls)
+    }else  {
+      return res.json(urls)
+    }
+  },true)
 };
 
 export const remove_file = async (req: Request, res: Response) => {
@@ -59,43 +27,4 @@ export const remove_file = async (req: Request, res: Response) => {
     console.log(e);
     res.status(500).json(e);
   }
-};
-
-export const upload = async (req: Request, res: Response) => {
-  let saveToPath = null;
-  let extension = null;
-  const busboy = Busboy({
-    headers: req.headers,
-    limits: {
-      fileSize: 5 * 1024 * 1024,
-    },
-  });
-  busboy
-    .on(
-      'file',
-      function (
-        _name: string,
-        file: NodeJS.ReadableStream,
-        info: { filename: string; encoding: string; mimeType: string }
-      ) {
-        const randomFilename = randomStr(32);
-        extension = info.mimeType.split('/')[1];
-        saveToPath = path
-          .join(os.tmpdir(), randomFilename + '.' + extension)
-          .toString();
-        file.pipe(fs.createWriteStream(saveToPath));
-      }
-    )
-    .on('finish', async function () {
-      try {
-        const urls = await uploadFileToStore(saveToPath, extension);
-        fs.unlink(saveToPath, () => {});
-        console.log(urls)
-        return urls;
-      } catch (e) {
-        console.error(e.toString());
-        return null;
-      }
-    });
-  return req.pipe(busboy);
 };
