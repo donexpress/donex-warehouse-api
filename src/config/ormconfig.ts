fb6d76135acfb396d7fd75be765d9fed739df9cf
YMC <ycuello85@gmail.com>
2023-10-27 14:58:28 -0400

    wip

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,23 +1,23 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import "dotenv/config";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
-  migrationsTableName: "migrations",
-  type: "postgres",
-  host: process.env.DB_HOST ?? "localhost",
-  port: isNaN(Number(process.env.DB_PORT))  ? 5432 : Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME ?? "postgres",
-  password: process.env.DB_PASSWORD ?? "postgres",
-  database: process.env.DB_NAME ?? "warehouse_dev",
-  // url: process.env.DATABASE_URL,
-  // extra: {
-  //   ssl: { rejectUnauthorized: false },
-  // },
+  migrationsTableName: 'migrations',
+  type: 'postgres',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: isNaN(Number(process.env.DB_PORT)) ? 5432 : Number(process.env.DB_PORT),
+  //username: process.env.DB_USERNAME ?? "postgres",
+  //password: process.env.DB_PASSWORD ?? "postgres",
+  //database: process.env.DB_NAME ?? "warehouse_dev",
+  url: process.env.DATABASE_URL,
+  extra: {
+    ssl: { rejectUnauthorized: false },
+  },
   synchronize: false,
   logging: false,
-  name: "default",
-  entities: ["src/**/*.model.ts"],
-  migrations: ["dist/**/migrations/**/*.js"],
+  name: 'default',
+  entities: ['src/**/*.model.ts'],
+  migrations: ['dist/**/migrations/**/*.js'],
   subscribers: [],
 });
