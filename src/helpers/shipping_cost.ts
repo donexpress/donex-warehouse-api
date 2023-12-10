6a92cdf82e3f6efdf2dc7fca6250506185ba8def
YMC <ycuello85@gmail.com>
2023-12-10 10:08:42 -0500

    wip

--- src/helpers/shipping_cost.ts
+++ src/helpers/shipping_cost.ts
@@ -6,29 +6,29 @@ export const calculate_cost = async (weight, carrier) => {
         fee = 2.1;
       }
 
-      if (weight > 0.501 && weight <= 1000) {
+      if (weight > 0.501 && weight <= 1.0) {
         fee = 2.3;
       }
 
-      if (weight > 1001 && weight <= 2000) {
+      if (weight > 1.001 && weight <= 2.0) {
         fee = 2.5;
       }
       break;
 
     case 'Redpack':
-      if (weight > 0 && weight <= 1000) {
+      if (weight > 0 && weight <= 1.0) {
         fee = 2.05;
       }
 
-      if (weight > 1001 && weight <= 3000) {
+      if (weight > 1.001 && weight <= 3.0) {
         fee = 2.15;
       }
 
-      if (weight > 3001 && weight <= 5000) {
+      if (weight > 3.001 && weight <= 5.0) {
         fee = 2.5;
       }
 
-      if (weight > 5001 && weight <= 30000) {
+      if (weight > 5.001 && weight <= 30.0) {
         fee = 3.0;
       }
       break;
